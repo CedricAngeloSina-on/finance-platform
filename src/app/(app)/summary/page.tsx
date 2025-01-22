3d0a095501a5ca0d4cadbe59ecb87a70d82330b6
Cedric <cao.sinaon@gmail.com>
2025-01-22 21:52:12 +0800

    fix: useSearchParams error

--- src/app/(app)/summary/page.tsx
+++ src/app/(app)/summary/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { PiggyBank, TrendingDown, TrendingUp } from "lucide-react";
 
+import { PiggyBank, TrendingDown, TrendingUp } from "lucide-react";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { api } from "~/trpc/react";
 
@@ -8,35 +9,33 @@ import { formatDateRange } from "~/lib/utils";
 import { SummaryCard } from "~/components/summary-card";
 import { BarChart } from "~/components/charts/bar-chart";
 
-export default function Summary() {
-  // const utils = api.useUtils();
+function SummaryContent() {
+  const searchParams = useSearchParams();
+  const from = searchParams.get("from") ?? undefined;
+  const to = searchParams.get("to") ?? undefined;
 
   const summary = api.summary.getSummary.useQuery({});
-
-  const params = useSearchParams();
-  const from = params.get("from") ?? undefined;
-  const to = params.get("to") ?? undefined;
-
   const dateRangeLabel = formatDateRange({ to, from });
+
   return (
     <div className="flex h-full flex-col gap-4">
       <div className="grid auto-rows-min gap-4 lg:grid-cols-3">
         <SummaryCard
-          title={"Remaining"}
+          title="Remaining"
           icon={PiggyBank}
           value={summary.data?.remainingAmmount}
           dateRange={dateRangeLabel}
           percentageChange={summary.data?.remainingChange}
         />
         <SummaryCard
-          title={"Income"}
+          title="Income"
           icon={TrendingUp}
           value={summary.data?.incomeAmmount}
           dateRange={dateRangeLabel}
           percentageChange={summary.data?.incomeChange}
         />
         <SummaryCard
-          title={"Expenses"}
+          title="Expenses"
           icon={TrendingDown}
           value={summary.data?.expensesAmount}
           dateRange={dateRangeLabel}
@@ -50,5 +49,13 @@ export default function Summary() {
         <div className="min-h-[300px] flex-1 rounded-xl"></div>
       </div>
     </div>
+  );
+}
+
+export default function Summary() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SummaryContent />
+    </Suspense>
   );
 }
